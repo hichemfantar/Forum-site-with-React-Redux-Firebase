129e6c201f6f60e41781a2392c2d1153082edb60
Hichem Fantar <mobyfreak123@gmail.com>
2020-08-19 00:15:10 +0100

    Add Routing and Redux store

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Dashboard from "./components/dashboard/Dashboard";
 import ProjectDetails from "./components/project/ProjectDetails";
+import SignIn from "./components/auth/SignIn";
+import SignUp from "./components/auth/SignUp";
+import CreateProject from "./components/project/CreateProject";
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
         <Switch>
           <Route exact path="/" component={Dashboard}></Route>
           <Route path="/project/:id" component={ProjectDetails}></Route>
+          <Route path="/signin" component={SignIn}></Route>
+          <Route path="/signup" component={SignUp}></Route>
+          <Route path="/createproject" component={CreateProject}></Route>
         </Switch>
       </div>
     </BrowserRouter>
